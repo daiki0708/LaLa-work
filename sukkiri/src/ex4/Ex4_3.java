638ca2dca498acbd5001b57fdb0f31a13466c05f
DESKTOP-7FI5EL3\user <ojj1652t7225c6g@gmail.com>
2022-05-23 16:27:37 +0900

    no message

---
+++ sukkiri/src/ex4/Ex4_3.java
@@ -0,0 +1,13 @@
+package ex4;
+
+public class Ex4_3 {
+
+	public static void main(String[] args) {
+	    int[] counts = null;
+	    float[] height = {171.3F, 175.0F};
+//	    System.out.println(counts[1]);
+	    System.out.println(height[2]);
+
+	}
+
+}
